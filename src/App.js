af37dd317c14a8bc3f1c103b07c61bf78cfe172e
Nevil Patel <93375041+NEVIL-PATEL@users.noreply.github.com>
2022-12-06 17:31:41 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import Home from './pages';
+import Product from './pages/product';
+import Contact from './pages/contact';
+import Signup from './pages/signup';
 import './App.css';
 
-function App() {
+function App() { 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <Router>
+      <Navbar />
+      <Routes>
+         <Route path='/' element={<Home/>} />
+         <Route path='/product' element={<Product/>} />
+         <Route path='/contact' element={<Contact/>} />
+         <Route path='/sign-up' element={<Signup/>} />
+      </Routes>
+      <Footer />
+    </Router>
+   
   );
 }
-
 export default App;
